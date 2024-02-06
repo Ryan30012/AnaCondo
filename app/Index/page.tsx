9246b1f0c7599d4f76d96b7d287a0b8f8a13382c
Suha AB <abubakr.suha@gmail.com>
2024-02-06 11:31:46 -0500

    commit signin and signup

---
+++ app/Index/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Index = () => {
+  return (
+    <div>
+        I am Index
+    </div>
+
+  )
+}
+
+export default Index
