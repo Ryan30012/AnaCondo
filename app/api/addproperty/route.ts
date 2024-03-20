55e53d4bfeac095d57b0b6ab624bd1002b2fbbec
Sisahga Phimmasone <80082494+Sisahga@users.noreply.github.com>
2024-03-20 03:23:04 -0400

    improved backend add property #26

--- app/api/addproperty/route.ts
+++ app/api/addproperty/route.ts
@@ -2,7 +2,7 @@ import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 
-export async function POST(request: Request): Promise<NextResponse> {
+export async function POST(request: Request) {
   try {
     const { name, unitCount, parkingCount, lockerCount, address } =
       await request.json();
@@ -13,9 +13,10 @@ export async function POST(request: Request): Promise<NextResponse> {
     const newBID = buildingCount + 1;
     const result =
       await sql`insert into buildings (bid, name, unitcount, parkingcount, lockercount, address) values (${newBID}, ${name}, ${unitCount}, ${parkingCount}, ${lockerCount}, ${address})`;
+
+    return NextResponse.json(name);
   } catch (e) {
     console.log({ e });
+    return NextResponse.json({ message: "Failed to add property" });
   }
-
-  return NextResponse.json({ message: "This is a POST request." });
 }
