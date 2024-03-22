992eb1afcc533d1ee8344fbd3c15862b9f0b33a6
Ryan30012 <ryan.sef1@gmail.com>
2024-03-21 21:21:12 -0400

    Unit Testing for MessageBoard

--- components/guestnavbar/GuestNavbar.tsx
+++ components/guestnavbar/GuestNavbar.tsx
@@ -6,23 +6,25 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div id="navbarMain" className="flex justify-between p-5">
-      <div className="navbarLogo">
-        <p className="navbarLogoTitle">
-          <Link href="/">AnaCondo</Link>
-        </p>
-      </div>
-      <div id="navbarAccountNoLogin" className="flex">
-        <Link href="/SignIn">
-          <button id="loginBtn" className="navButton">
-            Login
-          </button>
-        </Link>
-        <Link href="/SignUp">
-          <button id="signupBtn" className="navButton">
-            Signup
-          </button>
-        </Link>
+    <div>
+      <div id="navbarMain" className="flex justify-between p-5">
+        <div className="navbarLogo">
+          <p className="navbarLogoTitle">
+            <Link href="/">AnaCondo</Link>
+          </p>
+        </div>
+        <div id="navbarAccountNoLogin" className="flex">
+          <Link href="/SignIn">
+            <button id="loginBtn" className="navButton">
+              Login
+            </button>
+          </Link>
+          <Link href="/SignUp">
+            <button id="signupBtn" className="navButton">
+              Signup
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
