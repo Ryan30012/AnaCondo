9ffcf2d39c872061322f14293ff3bef25bb4776a
Suha AB <abubakr.suha@gmail.com>
2024-04-10 01:35:20 -0400

    implementing condo fees backend #64

--- app/CondoOwnerDashboard/page.tsx
+++ app/CondoOwnerDashboard/page.tsx
@@ -1,6 +1,30 @@
+import { getServerSession } from "next-auth";
 import CondoOwnerDashbaord from "./form";
 import React from "react";
+import { redirect } from "next/navigation";
+import { sql } from "@vercel/postgres";
 
 export default async function CondoDashbaord() {
-  return <CondoOwnerDashbaord />;
+  const session = await getServerSession();
+
+  // If not logged in, redirect to signin
+  if (!session?.user?.email) {
+    redirect("/SignIn");
+  }
+
+  // Retreive User type
+  var email = "";
+  if (session?.user?.email) email = session.user.email;
+  const userInfo = await sql`SELECT * FROM users WHERE Email = ${email}`;
+  const userType = userInfo.rows[0].accounttype;
+  
+  // Redirect if not condo owner
+  if (userType != "CONDO_OWNER") {
+    redirect("/SignIn");
+  }
+  
+  // Retrieve Condo Owner's units
+  // this will be passed as props to the Client Component (CondoOwnerDashboard)
+  const userUnits = await sql`SELECT * FROM condounits WHERE owner = ${userInfo.rows[0].uid}`;
+  return <CondoOwnerDashbaord userUnits={userUnits} />;
 }
