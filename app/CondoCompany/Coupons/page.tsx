ed55b1c144cb954463763d57282b2754c64110c8
s-vali <91510546+s-vali@users.noreply.github.com>
2024-04-10 14:59:11 -0400

    Update page.tsx

--- app/CondoCompany/Coupons/page.tsx
+++ app/CondoCompany/Coupons/page.tsx
@@ -118,7 +118,7 @@ export default async function CompanyCoupons() {
           {/* Coupons */}
 
           <div
-            ata-testid="coupon-table"
+            data-testid="coupon-table"
             className="relative overflow-x-auto shadow sm:rounded-lg"
           >
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
