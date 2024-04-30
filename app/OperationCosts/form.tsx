94c3605f4128c190f57607230cc88af6027001e7
Megan Coscia <meg.coscia@gmail.com>
2024-04-30 19:44:27 -0400

    rework file organization #32

--- app/OperationCosts/form.tsx
+++ app/OperationCosts/form.tsx
@@ -1,36 +1,80 @@
+"use client";
+
+import { FormEvent } from "react";
+import Form from './form';
+import EditCostsForm from "./editCosts";
 import { getServerSession } from "next-auth";
-import React from "react";
 import { redirect } from "next/navigation";
 import { sql } from "@vercel/postgres";
+import React, { useState } from 'react';
+import Link from "next/link";
 
-export default async function ViewOperationCosts() {
-    const session = await getServerSession();
+interface UnitOperations {
+    operationID: string;
+    budget: string;
+    cost: string;
+    unitID: string;
+    ownerID: string;
+}
 
-    // 1 - ENSURING CORRECT USER IS LOGGED IN
-    // If not logged in, redirect to signin
-    if (!session?.user?.email) redirect("/SignIn");
+export default function OperationCosts(props: any) {
+    const ownerID = props.ownderid;
+    const condoUnits = props.condounits;
+    const operations = props.operations;
 
-    // Retreive User type
-    var email = "";
-    if (session?.user?.email) email = session.user.email;
-    const userInfo = await sql`SELECT * FROM users WHERE Email = ${email}`;
-    const userType = userInfo.rows[0].accounttype;
+    const [childState, setChildState] = useState<UnitOperations>();
 
-    // Redirect if not condo owner
-    if (userType != "CONDO_MANAGEMENT_COMPANY") redirect("/SignIn");
+    // HANDLE BUTTON CLICK WHEN WANTING TO EDIT THE ROW
+    // const handleClick = (oid: any, budget: any, cost: any, cuid: any, ownerid: any): React.MouseEventHandler<HTMLButtonElement> => {      
+    //     <Link href="/EditOperations" state={{operationID: oid, budget: budget, cost: cost, unitID: cuid, ownerID: ownerid}}></Link>
+    // }
 
+    // create rows containing each property's info
+    const rows = [];
+    for(let i = 0; i < condoUnits.length; i++){
+        let temp = { operationID: operations[i].oid, 
+                        budget: operations[i].budget, 
+                        cost: operations[i].cost, 
+                        unitID: condoUnits[i].cuid,
+                        ownerID: operations[i].ownerid
+        };
+        setChildState(temp);
 
-    // 2 - RETRIEVING DATA FROM DATABASE
-    // Retrieve condo owner's user id for future retrieval
-    const userQuery = await sql`SELECT * FROM users WHERE email = ${session.user.email}`;
-    const ownerID = userQuery.rows[0].uid;
+        /*let operationID = operations[i].oid, budget = operations[i].budget, cost = operations[i].cost, unitID= condoUnits[i].cuid, ownerID= operations[i].ownerid;
+                <!--<button 
+                    onClick={handleClick(operations[i].oid, operations[i].budget, operations[i].cost, condoUnits[i].cuid, operations[i].ownerid)}>
+                        Modify
+                </button>-->
+        */
+        const row = (
+            <tr>
+                <td className="border-b border-slate-100 p-4 pl-8">{condoUnits[i].cuid}</td>
+                <td className="border-b border-slate-100 p-4 pl-8">{operations[i].budget}</td>
+                <td className="border-b border-slate-100 p-4 pl-8">{operations[i].cost}</td>
+                <Link 
+                    className="bg-lime-600 hover:bg-black hover:font-semibold text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    to={{pathname: "/EditOperations", state: {childState}}}>Modify</Link>
+            </tr>
+        );
+        rows.push(row);
+    }
 
-    // Retrieve condo units
-    const condoUnitsQuery = await sql`SELECT * FROM condounits WHERE owner = ${ownerID}`;
-    const condoUnits = condoUnitsQuery.rows;
-
-    // Retrieve operations associated to condo units
-    const operationsQuery = await sql`SELECT * FROM operations WHERE ownerid = ${ownerID}`;
-    const operations = operationsQuery.rows;
-
+    // display html
+    return (
+        <div className="relative rounded-xl overflow-auto container mx-auto">
+            <div className="shadow-sm overflow-hidden my-8">
+                <table className="border-collapse table-auto w-full m-3">
+                    <thead>
+                        <tr className="relative bg-lime-500 rounded p-4">
+                            <th className="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-600 text-left">Property</th>
+                            <th className="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-600 text-left">Budget</th>
+                            <th className="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-600 text-left">Cost</th>
+                            <th className="border-b font-medium p-4 pl-8 pt-0 pb-3 text-slate-600 text-left">Modify Fees</th>
+                        </tr>
+                    </thead>
+                    <tbody>{rows}</tbody>
+                </table>
+            </div>
+        </div>
+    );
 }
