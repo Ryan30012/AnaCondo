c1bd5d43af9d93b3ac26f7e8415c8ceb5bb8fffc
Jason-817 <jasonnovio@hotmail.ca>
2024-04-29 20:58:12 -0400

    Implemented the page for condo unit cost that takes the elements from the route.ts file from user story 423, #30

---
+++ app/CondoUnitCosts/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CondoCostsForm from './form';
+
+const CondoUnitCostPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Set Condo Unit Costs</h1>
+      <CondoCostsForm />
+    </div>
+  );
+};
+
+export default CondoUnitCostPage;
