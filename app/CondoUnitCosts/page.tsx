c4924c44128a7401aeff43deafd914186006b932
Megan Coscia <meg.coscia@gmail.com>
2024-04-29 23:11:11 -0400

    styling general html format of condo unit costs page #30

--- app/CondoUnitCosts/page.tsx
+++ app/CondoUnitCosts/page.tsx
@@ -3,9 +3,11 @@ import CondoCostsForm from './form';
 
 const CondoUnitCostPage: React.FC = () => {
   return (
-    <div>
-      <h1>Set Condo Unit Costs</h1>
-      <CondoCostsForm />
+    <div className="flex h-screen">
+      <div className="flex-1 w-full max-w-xs mx-auto">
+        <h1 className="font-bold text-3xl mb-3 pt-10 mt-10">Set Condo Unit Costs</h1>
+        <CondoCostsForm />
+      </div>
     </div>
   );
 };
